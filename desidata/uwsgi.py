6718bcded367b490d0344ac51444ea5987a430ef
padamsethia <padamsethia5@gmail.com>
2017-11-30 18:25:10 +0530

    Updated uwsgi run script

--- desidata/uwsgi.py
+++ desidata/uwsgi.py
@@ -2,3 +2,6 @@ from app import app as application
  
 if __name__ == "__main__":
 	application.run(host="0.0.0.0", debug=True )
+
+# RUN COMMAND
+# uwsgi --http :8000 --wsgi-file /h/home/deploy/Desidata/desidata/uwsgi.py
